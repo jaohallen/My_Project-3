ae265d069798b5a7c495e1c6e43020beb7517f8a
Jao Hallen <jaohallenbandos@gmail.com>
2017-11-10 16:43:15 +0800

    Added a url for creating choices for a question

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.IndexView.as_view(), name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.IndexView.as_view(),
+        name='vote'),
     url(r'^create/$', views.CreateQuestionView.as_view(), name='create'),
-    url(r'^(?P<pk>[0-9]+)/update/$', views.UpdateQuestionView.as_view(), name='editquestion'),
+    url(r'^(?P<pk>[0-9]+)/update/$', views.UpdateQuestionView.as_view(),
+        name='editquestion'),
+    url(r'^(?P<pk>[0-9]+)/addchoice/$', views.CreateChoiceView.as_view(),
+        name='addchoice'),
 ]
