b172b839ab2d4ed6907d42e45b89e48b2272a098
Jao Hallen <jaohallenbandos@gmail.com>
2017-11-10 16:13:06 +0800

    Added a url link for update question

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.IndexView.as_view(), name='vote'),
     url(r'^create/$', views.CreateQuestionView.as_view(), name='create'),
+    url(r'^create/$', views.UpdateQuestionView.as_view(), name='editquestion'),
 ]
