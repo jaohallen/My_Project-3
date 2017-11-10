5798cc83b544b017a9278c9e073315e0562ecc3f
Jao Hallen <jaohallenbandos@gmail.com>
2017-11-10 14:23:52 +0800

    Added admin interface for Question model

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.models import Question
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
