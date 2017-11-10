25f6bf3025a99cabf48fe783da1983973acb131a
Jao Hallen <jaohallenbandos@gmail.com>
2017-11-10 16:20:03 +0800

    Edited update question url

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.IndexView.as_view(), name='vote'),
     url(r'^create/$', views.CreateQuestionView.as_view(), name='create'),
-    url(r'^update/$', views.UpdateQuestionView.as_view(), name='editquestion'),
+    url(r'^(?P<pk>[0-9]+)/update/$', views.UpdateQuestionView.as_view(), name='editquestion'),
 ]
