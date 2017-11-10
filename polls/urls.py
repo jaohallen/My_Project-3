6b879c26c9bfde345db48160a85dde2bd8d2551e
Jao Hallen <jaohallenbandos@gmail.com>
2017-11-10 15:48:51 +0800

    Added a url to create question

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.IndexView.as_view(), name='vote'),
+    url(r'^create/$', views.CreateQuestionView.as_view(), name='create'),
 ]
